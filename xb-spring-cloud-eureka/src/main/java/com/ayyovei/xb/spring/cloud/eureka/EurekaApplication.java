ec699ea5e08a2b7b43015372417ce4de2aeb7139
chenxiaobin <xb230805@163.com>
2019-05-28 01:03:14 +0800

    spring cloud demo

---
+++ xb-spring-cloud-eureka/src/main/java/com/ayyovei/xb/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.ayyovei.xb.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenxiaobin
+ * @description
+ * @create 2019/5/28
+ */
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
